1fb0482c8ca5bd755f759d77ca50994705d827b6
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-15 12:45:55 -0400

    adding client utils

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -4,7 +4,6 @@ class AuthService {
   getProfile() {
     return decode(this.getToken());
   }
-
   loggedIn() {
     const token = this.getToken();
     // If there is a token and it's not expired, return `true`
