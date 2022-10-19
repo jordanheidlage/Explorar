6e44d9395459b6f45f828dad3c89763b353e9e29
0magical <marilynventura@outlook.com>
2022-10-19 14:57:56 -0400

    delete

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,12 @@
-import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
-import { useState } from 'react';
-// import Greece from './pages/Greece';
-// import Indonesia from './pages/Indonesia';
-// import Italy from './pages/Italy';
-// import Japan from './pages/Japan';
-// import Seychelles from './pages/Seychelles';
-// import Turkey from './pages/Turkey';
+
+
 
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("Home")
+
   return (
     <div className="App">
-      <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      {
-        (() => {
-          switch (currentPage) {
-            case "Home":
-              return <Home />
-
-            // case "":
-            //   // return < />
-
-            // case "":
-            //   // return < />
-
-            // case "":
-            //   // return < />
-
-            default:
-              return <Home />
-
-          }
-        })()
-      }
+    
     </div>
   );
 }
