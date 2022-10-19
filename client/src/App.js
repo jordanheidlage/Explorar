a99b2613a9e7d3f5b35f25dbb8bc1e288ad5fa8d
0magical <marilynventura@outlook.com>
2022-10-19 15:03:59 -0400

    added nav bar compenent

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 
 
 
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-    
+      <Navbar/>
     </div>
   );
 }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar () {
+    return (
+<>
+<h1>this is nav</h1>
+</>
+    )
+}
+
+export default Navbar;
