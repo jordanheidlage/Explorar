179b40bd2a3fc8a7188e881ca0bc127a68bcd473
Lucas Saaler <lucassaaler00@gmail.com>
2022-10-19 20:53:29 -0400

    Set up React to work and added save button boiler plate

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
+import Italy from './pages/Italy'
 import Home from './Home';
 import { useState } from 'react';
 // import Greece from './pages/Greece';
@@ -9,17 +19,42 @@ import { useState } from 'react';
 // import Seychelles from './pages/Seychelles';
 // import Turkey from './pages/Turkey';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   const [currentPage, setCurrentPage] = useState("Home")
   return (
+    <ApolloProvider client={client}>
     <div className="App">
       <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
       {
         (() => {
           switch (currentPage) {
             case "Home":
-              return <Home />
+              return <Italy />
 
             // case "":
             //   // return < />
@@ -31,12 +66,13 @@ function App() {
             //   // return < />
 
             default:
-              return <Home />
+              return <Italy />
 
           }
         })()
       }
     </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Italy.js
+++ client/src/pages/Italy.js
@@ -1,3 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { SAVE_LOCATION } from '../utils/mutations';
+import { QUERY_ME } from '../utils/queries'
+import Auth from '../utils/auth';
+import { saveLocationIds, getSavedLocationIds } from '../utils/localStorage';
+
+const Italy = () => {
+
+  const [saveLocation, { error }] = useMutation(SAVE_LOCATION, {
+    update(cache, { data: { saveLocation } }) {
+      try {
+        const { me } = cache.readQuery({ query: QUERY_ME });
+
+        cache.writeQuery({
+          query: QUERY_ME,
+          data: { me: saveLocation.savedLocations },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  });
+
+  const handleSaveLocation = async () => {
+    const locationToSave = {
+      name: "Italy",
+      locationId: "1",
+      image: "placeholder"
+    }
+
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      console.log({locationToSave})
+      const { data } = await saveLocation({
+        variables: {
+          ...locationToSave
+        },
+      });
+      // setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return(
 <>
   {/* Hello world */}
   <div className="awesome" style={{ border: "1px solid red" }}>
@@ -40,7 +93,7 @@
               <a href="#flights">Flights to catch</a>
             </li>
             <li id="number3">
-              <a href="#save">
+              <a href="#save" onClick={() => handleSaveLocation()}>
                 Save
                 <i className="fa-solid fa-bookmark" />
               </a>
@@ -258,3 +311,6 @@
   </footer>
   {/*footer end*/}
 </>
+)}
+
+export default Italy
