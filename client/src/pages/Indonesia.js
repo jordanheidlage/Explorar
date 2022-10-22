c7ad1869576e12d2232f5a2bfc2a41d43d1bd962
Lucas Saaler <lucassaaler00@gmail.com>
2022-10-22 12:53:04 -0400

    Prepared boiler plate for other pages and moved css

--- client/src/pages/Indonesia.js
+++ client/src/pages/Indonesia.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { SAVE_LOCATION } from '../utils/mutations';
+import { QUERY_ME } from '../utils/queries'
+import Auth from '../utils/auth';
+import { saveLocationIds, getSavedLocationIds } from '../utils/localStorage';
 
 function Indonesia () {
     return (
         <>
         
-        
+        {/*html here*/}
         
         </>
     )
