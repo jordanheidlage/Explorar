49416510109906fffbcb0abe90e680a490f56049
0magical <marilynventura@outlook.com>
2022-10-24 17:14:47 -0400

    changed features for profile and navbar

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,94 +9,115 @@ import { removeLocationId } from '../utils/localStorage';
 
 
 const Profile = () => {
-    const [userData, setUserData] = useState({});
-    const { loading, data } = useQuery( QUERY_ME );
-    const [removeLocation, { error }] = useMutation(REMOVE_LOCATION, {
-      // All returning data from Apollo Client queries/mutations return in a `data` field, followed by the the data returned by the request
-      update(cache, { data: { removeLocation } }) {
-        try {
-          const { me } = cache.readQuery({ query: QUERY_ME });
-  
-          cache.writeQuery({
-            query: QUERY_ME,
-            data: { me: removeLocation.savedLocations },
-          });
-        } catch (e) {
-          console.error(e);
-        }
-      },
-    }); 
-
-    useEffect(() => {
-      const getUserData = async () => {
-        try {
-          if (data){
-            console.log(data.me)
-            setUserData(data.me);
-          }
-        } catch (err) {
-          console.error(err);
-        }
-      };
-  
-      getUserData();
-    }, [data]);
-  
-    const handleDeleteLocation = async (locationId) => {
-      console.log(locationId)
-      const token = Auth.loggedIn() ? Auth.getToken() : null;
-  
-      if (!token) {
-        return false;
-      }
+  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeLocation, { error }] = useMutation(REMOVE_LOCATION, {
+    // All returning data from Apollo Client queries/mutations return in a `data` field, followed by the the data returned by the request
+    update(cache, { data: { removeLocation } }) {
       try {
-        const {updatedUser} = await removeLocation({
-          variables: {
-            locationId
-          }
+        const { me } = cache.readQuery({ query: QUERY_ME });
+
+        cache.writeQuery({
+          query: QUERY_ME,
+          data: { me: removeLocation.savedLocations },
         });
-  
-        setUserData(updatedUser);
-        
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  });
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        if (data) {
+          console.log(data.me)
+          setUserData(data.me);
+        }
       } catch (err) {
         console.error(err);
       }
     };
-    
-    const userDataLength = userData? Object.keys(userData).length: 0;
-    // if data isn't here yet, say so
-    if (!userDataLength) {
-      return <h2>LOADING...</h2>;
+
+    getUserData();
+  }, [data]);
+
+  const handleDeleteLocation = async (locationId) => {
+    console.log(locationId)
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
     }
+    try {
+      const { updatedUser } = await removeLocation({
+        variables: {
+          locationId
+        }
+      });
 
-    return(
-        <>
+      setUserData(updatedUser);
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const userDataLength = userData ? Object.keys(userData).length : 0;
+  // if data isn't here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col>
+            {userData.savedLocations.map((location) => {
+              return (
+                <Card key={location.locationId} border='dark'>
+                  {location.image ? <Card.Img src={location.image} alt={`The cover for ${location.name}`} variant='top' /> : null}
+                  <Card.Body>
+                    <Card.Title><a href={location.name}>{location.name}</a></Card.Title>
+                    <Button className='btn-block btn-danger' onClick={() => handleDeleteLocation(location.locationId)}>
+                      Delete this Location!
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Col>
+        ))}
+      </Row>
 
 
-         <CardColumns>
-          {userData.savedLocations.map((location) => {
-            return (
-              <Card key={location.locationId} border='dark'>
-                {location.image ? <Card.Img src={location.image} alt={`The cover for ${location.name}`} variant='top' /> : null}
-                <Card.Body>
-                  <Card.Title>{location.name}</Card.Title>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteLocation(location.locationId)}>
-                    Delete this Location!
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardColumns> 
-        {/* HTML here */}
+{/* 
+      <CardColumns>
+        {userData.savedLocations.map((location) => {
+          return (
+            <Card key={location.locationId} border='dark'>
+              {location.image ? <Card.Img src={location.image} alt={`The cover for ${location.name}`} variant='top' /> : null}
+              <Card.Body>
+                <Card.Title><a href={location.name}>{location.name}</a></Card.Title>
+                <Button className='btn-block btn-danger' onClick={() => handleDeleteLocation(location.locationId)}>
+                  Delete this Location!
+                </Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </CardColumns> */}
+
+      {/* HTML here */}
 
 
 
 
 
 
-        </>
-    )
+    </>
+  )
 }
 
 export default Profile
